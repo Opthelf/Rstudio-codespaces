7a689ed8f5ffe3c8e7ef05ede64f8e5b1bb132ce
Opthelf <125304236+Opthelf@users.noreply.github.com>
2024-10-04 12:12:14 +0000

    Ex2

--- DataE/Ex2.r
+++ DataE/Ex2.r
@@ -1,3 +1,9 @@
-Dataset <- read.table("WineQuality.csv",header=TRUE,row.names=1, sep=';',dec='.')
+Dataset <- read.table("WineQuality.csv",header=TRUE, sep=';',dec='.')
 
+#1)13 variables et 6497 observations
 
+#2) les variables sont quantitatives
+#3)
+summary(Dataset)
+is.factor(Dataset$quality)
+is.factor(Dataset$color)        
